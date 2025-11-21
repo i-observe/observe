c4cf28647aa501af3913631467e75a157cd40d1a
Mirai <mirai@Mirais-MacBook-Air.local>
2025-11-21 16:51:24 +0545

    test

--- particles-config.js
+++ particles-config.js
@@ -1,25 +1,25 @@
-particlesJS('particles-js', {
-    particles: {
-        number: { value: 100, density: { enable: true, value_area: 800 } },
-        color: { value: "#ffffff" },
-        shape: { type: "circle" },
-        opacity: {
-            value: 1,
-            random: true,
-            anim: { enable: true, speed: 1, opacity_min: 0.3, sync: false }
-        },
-        size: { value: 4, random: true, anim: { enable: true, speed: 4, size_min: 1, sync: false } },
-        line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.7, width: 1.5 },
-        move: { enable: true, speed: 0.8, direction: "none", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false } }
-    },
-    interactivity: {
-        detect_on: "window",  // <-- CHANGE THIS from "canvas" to "window"
-        events: {
-            onhover: { enable: true, mode: "repulse" },
-            onclick: { enable: true, mode: "push" },
-            resize: true
-        },
-        modes: { repulse: { distance: 60, duration: 0.6 }, push: { particles_nb: 4 } }
-    },
-    retina_detect: true
-});
+// particlesJS('particles-js', {
+//     particles: {
+//         number: { value: 100, density: { enable: true, value_area: 800 } },
+//         color: { value: "#ffffff" },
+//         shape: { type: "circle" },
+//         opacity: {
+//             value: 1,
+//             random: true,
+//             anim: { enable: true, speed: 1, opacity_min: 0.3, sync: false }
+//         },
+//         size: { value: 4, random: true, anim: { enable: true, speed: 4, size_min: 1, sync: false } },
+//         line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.7, width: 1.5 },
+//         move: { enable: true, speed: 0.8, direction: "none", random: true, straight: false, out_mode: "out", bounce: false, attract: { enable: false } }
+//     },
+//     interactivity: {
+//         detect_on: "window",  // <-- CHANGE THIS from "canvas" to "window"
+//         events: {
+//             onhover: { enable: true, mode: "repulse" },
+//             onclick: { enable: true, mode: "push" },
+//             resize: true
+//         },
+//         modes: { repulse: { distance: 60, duration: 0.6 }, push: { particles_nb: 4 } }
+//     },
+//     retina_detect: true
+// });
